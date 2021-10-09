2e856c2fcbd2967fdf59dabaa7c97a3d34ae6a8b
unknown <61252778+Gamapro@users.noreply.github.com>
2021-10-09 15:49:00 -0500

    Update SegmentTree_class.cpp
    ..

--- C++/CompetitiveProgramming/Apoyo/SegmentTree_class.cpp
+++ C++/CompetitiveProgramming/Apoyo/SegmentTree_class.cpp
@@ -1,13 +1,66 @@
-class SegmentTreeValUpd{
+/******************************************************/
+/*                      RMQ                           */
+/******************************************************/
+
+class STValUpd{
 public:
-		
+	
+	// SegmentTreeValUpd
+	// RMQ
 	// Update value, query range
 		
 	vector<ll> v,st;
 	int n;
 	
-	SegmentTreeValUpd(){}
-	SegmentTreeValUpd(int _n) : n(_n) {
+	STValUpd(){}
+	STValUpd(int _n) : n(_n) {
+		v.assign(n,0); st.assign(2*n+5,0);
+	}
+	
+	void init(int _n){
+		n = _n; v.assign(n,0); st.assign(2*n+5,0);
+	}
+	
+	void read(){ for(int i=0;i<n;i++) cin>>v[i]; }
+
+	void build(){
+	    for(int i=0;i<n;i++){ st[i+n]=v[i]; }
+	    for(int i=n-1;i>0;i--){ st[i]=max(st[i*2],st[i*2+1]); }
+	}
+	
+	void update(int index,ll val){
+	    st[index+=n]=val;
+	    while(index/=2){ st[index]=max(st[index*2],st[index*2+1]); }
+	}
+	
+	ll query(int L,int R){  /// rango [ L , R )
+	    ll ans=0;
+	    for(L+=n,R+=n;L<R;L/=2,R/=2){
+	        if(L&1)ans=max(ans,st[L++]);
+	        if(R&1)ans=max(ans,st[--R]);
+	    }
+	    return ans;
+	}
+
+};
+
+
+/******************************************************/
+/*                 Range Sum                          */
+/******************************************************/
+
+class STValUpd{
+public:
+	
+	// SegmentTreeValUpd
+	// RANGE SUM
+	// Update value, query range
+		
+	vector<ll> v,st;
+	int n;
+	
+	STValUpd(){}
+	STValUpd(int _n) : n(_n) {
 		v.assign(n,0); st.assign(2*n+5,0);
 	}
 	
@@ -30,24 +83,26 @@ public:
 	ll query(int L,int R){  /// rango [ L , R )
 	    ll ans=0;
 	    for(L+=n,R+=n;L<R;L/=2,R/=2){
-	        if(L%2)ans+=st[L++];
-	        if(R%2)ans+=st[--R];
+	        if(L&1)ans+=st[L++];
+	        if(R&1)ans+=st[--R];
 	    }
 	    return ans;
 	}
 
 };
 
-class SegmentTreeRangeUpd{
+class STRangeUpd{
 public:
-		
+	
+	// SegmentTreeRangeUpd
+	// RANGE SUM
 	// Update range, query value		
 	
 	vector<ll> v,st;
 	int n;
 	
-	SegmentTreeRangeUpd(){}
-	SegmentTreeRangeUpd(int _n) : n(_n) {
+	STRangeUpd(){}
+	STRangeUpd(int _n) : n(_n) {
 		v.assign(n,0); st.assign(2*n+5,0);
 	}
 	
