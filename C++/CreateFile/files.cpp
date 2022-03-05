223b48e0a0d9da738991bfb1d6c79f9ecf7fb05b
unknown <61252778+Gamapro@users.noreply.github.com>
2022-03-05 14:15:54 -0600

    ffff

---
+++ C++/CreateFile/files.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fi first
+#define se second
+#define pb push_back
+#define INF (ll)1e18 +5
+#define all(a) a.begin(),a.end()
+#define bitOn(i,j) ((i)&((ll)1<<j))
+#define setBit(i,j) i=((i)|((ll)1<<j))
+#define forn(i,n) for(int i=0;i<n;i++)
+#define dbg(x) cout<<#x"="<<x<<'\n';
+#define dbg2(x,y) cout<<#x"="<<x<<" "<<#y"="<<y<<'\n';
+#define dbg3(x,y,z) cout<<#x"="<<x<<" "<<#y"="<<y<<" "<<#z"="<<z<<'\n';
+#define MOD 1000000007
+//更新前缀最大值
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<vi> vvi;
+typedef vector<vll> vvll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+ 
+ll t,n,k,m,h,a,b,c,x,y,q;
+
+int main(){
+    cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
+    
+    ofstream out ("test.txt");
+
+    out<<"1\n100000\n";
+    n = 100000;
+    forn(i,n-1) out<<0<<' ';
+    out<<1000000000;
+
+    out.close();  
+	
+}
+
+
+
+
+
+
